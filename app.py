fe9f41e6e2bfe441e91543cc62e0161be0848fc6
apgearhart1 <apgearhart1@gmail.com>
2020-10-01 19:45:17 -0500

    Fix GET, PUT, POST requests, all follow given format

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import json
 from collections import OrderedDict
 from datetime import datetime, timedelta, date
 import time
+import sys
 # Initialize Flask App
 app = Flask(__name__)
 # Initialize Firestore DB
@@ -16,13 +17,14 @@ db = firestore.client()
 COL_TELEMETRY = db.collection('telemetry')
 COL_BUFFER = db.collection('buffer')
 DATABASE_FORMAT = open('database_format.json', 'r')
+BUFFER = open('d.json', 'r')
 TJSON = './dataToCar.json'
-sensors = ["battery_current", "battery_voltage"]
 
 db_json = json.load(DATABASE_FORMAT)
- 
+buffer_json = json.load(BUFFER)
 
-
+SENSORS = ["battery_voltage", "battery_current", "battery_temperature", "bms_fault", "gps_time", "gps_lat","gps_lon",
+"gps_velocity_east", "gps_velocity_north", "gps_velocity_up", "gps_speed", "solar_voltage", "solar_current", "motor_speed"]
 
 
 def file_size(file_path):
@@ -33,26 +35,34 @@ def file_size(file_path):
         return os.stat(file_path)
 
 
-@app.route('/document', methods=['PUT'])
-def update():
+@app.route('/document/<date>', methods=['PUT'])
+def update(date):
     """
         update() : Update document in Firestore collection with request body
         Ensure you pass a custom ID as part of json body in post request
         e.g. json={'id': '1', 'title': 'Write a blog post today'}
     """
-    buffer = dict()
     dateTimeObj = datetime.now()
     timestampStr = dateTimeObj.strftime("%Y-%m-%d")
-    buffer = {"telemetry":{}}
-    with open(TJSON) as json_data:
-        fromCar = json.load(json_data)
-        buffer['telemetry'].update(fromCar)
+    collections = COL_TELEMETRY.document(date).collections()
     try:
         print("Updating buffer")
-        COL_TELEMETRY.document(timestampStr).update(buffer['telemetry'])
+        for col, sensor in zip(collections, SENSORS):
+            data_per_collection = dict()
+            for sec in buffer_json.keys():
+                data_per_collection[sec] = buffer_json[sec][sensor]["value"]
+            col.document("0").update({
+                "seconds":
+                    data_per_collection
+                
+            })
         print("Data was successfully uploaded")
+        return "Success", 202
     except Exception as e:
-        return f"An Error Occured: {e}"
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        return f"An Error Occured: {e}", 400
 
 
 @app.route('/document', methods=['POST'])
@@ -85,13 +95,14 @@ def read(date):
     dateFormat = "%Y-%m-%d"
 
     try:
-        datetime.strptime(date, dateFormat)
-        # Check if ID was passed to URL query  
-
+        # datetime.strptime(date, dateFormat)
+        data = dict()
         
-        data = COL_TELEMETRY.document(date).collection("battery_current").stream()
-
-        return jsonify(data.to_dict), 200
+        collections = COL_TELEMETRY.document(date).collections()
+        for col, sensor in zip(collections, db_json.keys()):
+            for doc in col.stream():
+                data[sensor] = doc.to_dict()
+        return jsonify(data), 200
     except Exception as e:
         return f"An Error Occured: {e}", 404
 
